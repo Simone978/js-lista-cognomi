3ef9d72bd60006000dec2c7bb00a1c6d3cfe6c96
Simone <simonelega.sl@gmail.com>
2020-01-08 16:50:02 +0100

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 
+// Chiedi all’utente il cognome,
+var cognome = prompt('inserisci il tuo cognome');
+
+// inseriscilo in un array con altri cognomi,
+var arrayCognomi = ['Lega', 'Mauceri', 'Montalto', 'Cannata', 'Caruso'];
+
+arrayCognomi.push(cognome);
+arrayOrdinato = arrayCognomi.sort();
+
+// stampa la lista ordinata alfabeticamente (in html ul > li).
+var i = 0;
+var posizione = 0;
+while (i < arrayOrdinato.length ) {
+  document.getElementById('lista_cognomi').innerHTML += '<li>' + arrayOrdinato[i] + '</li>';
+  // Scrivi anche la posizione della lista in cui il nuovo utente si trova.
+  if (arrayOrdinato[i]==cognome){
+  document.getElementById('posizione').innerHTML = i+1;
+    }
+  i++;
+
+}
+document.getElementById('lista_cognomi').classList.add('no_style');
